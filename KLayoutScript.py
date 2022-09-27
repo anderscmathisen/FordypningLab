849a976a66099c37e5e77acb3ef87d54a45da102
Anders Christian Mathisen <anderscm@mit.edu>
2022-09-27 16:53:39 +0200

    added support for drawing etchmask 2.4 um larger than all features

--- KLayoutScript.py
+++ KLayoutScript.py
@@ -85,20 +85,33 @@ class Hand:
         self.horizontal_bus_height: int = 50 * scale
         self.scale = scale
 
-    def draw_base(self):
+    def draw_base(self, etchmask=False):
         """Draws the base horizontal bus bar and vertical bussbar with parameters that are fixed in __init__"""
+
+        if etchmask:
+            offset_etchmask = 2.4 * self.scale
+
+        else:
+            offset_etchmask = 0
+
         self.top.shapes(self.layer).insert(
-            MyBox(self.start_pos, self.width, self.horizontal_bus_height)
+            MyBox(
+                self.start_pos.offset(-offset_etchmask, -offset_etchmask),
+                self.width + 2 * offset_etchmask,
+                self.horizontal_bus_height + 2 * offset_etchmask,
+            )
         )
         self.top.shapes(self.layer).insert(
             MyBox(
-                self.start_pos.offset_x(self.width / 2 - self.vertical_bus_width / 2),
-                self.vertical_bus_width,
-                self.height,
+                self.start_pos.offset_x(
+                    self.width / 2 - self.vertical_bus_width / 2
+                ).offset(-offset_etchmask, -offset_etchmask),
+                self.vertical_bus_width + 2 * offset_etchmask,
+                self.height + 2 * offset_etchmask,
             )
         )
 
-    def draw_fingers(self):
+    def draw_fingers(self, etchmask=False):
         """Calculates how many fingers fit on the LED, and draws them"""
         num_fingers = int(
             (self.height - self.horizontal_bus_height)
@@ -106,36 +119,62 @@ class Hand:
         )
 
         for i in range(num_fingers):
-
-            finger_pos = self.start_pos.offset_y(
+            if etchmask:
+                offset_etchmask = 2.4 * self.scale
+            else:
+                offset_etchmask = 0
+            finger_pos = self.start_pos.offset(
+                -offset_etchmask,
                 i * (self.finger_width + self.finger_pitch)
                 + self.horizontal_bus_height
                 + self.finger_pitch
+                - offset_etchmask,
             )
 
-            finger = Finger(finger_pos, self.width, self.finger_width)
+            finger = Finger(
+                finger_pos,
+                self.width + 2 * offset_etchmask,
+                self.finger_width + 2 * offset_etchmask,
+            )
             finger.draw(self.top, self.layer)
 
     def draw(self):
         """Method that draws the entire LED hand"""
         self.draw_base()
         self.draw_fingers()
-        self.draw_enclosing_box()
+        # self.draw_enclosing_box()
         self.draw_Info()
+
+    def draw_etch_mask(self):
+        self.draw_base(etchmask=True)
+        self.draw_fingers(etchmask=True)
+        self.draw_Info(etchmask=True)
 
     def __str__(self):
         return f"Finger width: {self.finger_width/self.scale}\nFinger Pitch: {self.finger_pitch//self.scale}"
 
-    def draw_Info(self):
-        gen = pya.TextGenerator.default_generator()
-        w = self.finger_width
-        p = self.finger_pitch
+    def draw_Info(self, etchmask=False):
+        if etchmask:
+            self.top.shapes(self.layer).insert(
+                MyBox(
+                    self.start_pos.offset(
+                        -30 * self.scale - 0.2 * self.width,
+                        int(self.height * 1.2 - 20 * self.scale),
+                    ),
+                    self.width * 1.65,
+                    100 * self.scale,
+                )
+            )
+            return
+        else:
 
-        region = gen.text(str(self), gen.dbu(), 100 // 2)
-        pos = self.start_pos.offset_y(int(self.height * 1.2))
-        t = pya.Trans(*pos)
-        region.transform(t)
-        self.top.shapes(self.layer).insert(region)
+            gen = pya.TextGenerator.default_generator()
+
+            region = gen.text(str(self), gen.dbu(), 75)
+            pos = self.start_pos.offset(-self.width * 0.2, int(self.height * 1.2))
+            t = pya.Trans(*pos)
+            region.transform(t)
+            self.top.shapes(self.layer).insert(region)
 
     def draw_enclosing_box(self):
 
@@ -146,16 +185,14 @@ class Hand:
                     -self.enclosing_box_fraction / 2 * self.height,
                 ),
                 self.width * (1 + self.enclosing_box_fraction),
-                self.height * (1 + self.enclosing_box_fraction),
+                self.height * (1 + self.enclosing_box_fraction) * 1.2,
             )
         )
 
 
-if __name__ == "__main__":
+def draw_grid(layout, top):
 
-    layout = pya.Layout()
-
-    top = layout.create_cell("TOP")
+    # top = layout.create_cell("TOP")
     layer = layout.layer(0, 0)
     layer_txt = layout.layer(1, 0)
     layer_box = layout.layer(2, 0)
@@ -165,15 +202,15 @@ if __name__ == "__main__":
     LED_widht = 1000 * scale  # microns
     LED_heigth = 1000 * scale  # microns
 
-    LED_spacing_x = 5000 * scale  # microns
-    LED_spacing_y = 5000 * scale  # microns
+    LED_spacing_x = 1600 * scale  # microns
+    LED_spacing_y = 1600 * scale  # microns
 
     finger_widths = np.array([4, 4.5, 5, 5.5]) * scale  # microns
-    finger_pitches = np.array([50, 100, 150, 200]) * scale  # microns
+    finger_pitches = np.array([50, 100, 150, 230]) * scale  # microns
 
     startpos = Point(0, 0)
 
-    enclosing_box_fraction = 0.6
+    enclosing_box_fraction = 0.3
 
     for x in range(4):
         for y in range(4):
@@ -200,6 +237,104 @@ if __name__ == "__main__":
 
             single_hand.draw()
 
+
+def draw_single_led():
+    layout = pya.Layout()
+
+    top = layout.create_cell("TOP")
+    layer = layout.layer(0, 0)
+    layer_txt = layout.layer(1, 0)
+    layer_box = layout.layer(2, 0)
+
+    scale = 1000
+
+    LED_widht = 1000 * scale  # microns
+    LED_heigth = 1000 * scale  # microns
+
+    LED_spacing_x = 5000 * scale  # microns
+    LED_spacing_y = 5000 * scale  # microns
+
+    hand_point = Point(0, 0)
+
+    enclosing_box_fraction = 0.1
+
+    finger_width = 4 * scale
+    finger_pitch = 36 * scale
+
+    single_hand = Hand(
+        top,
+        layer,
+        layer_box,
+        enclosing_box_fraction,
+        hand_point,
+        finger_width,
+        finger_pitch,
+        LED_widht,
+        LED_heigth,
+        scale,
+    )
+
+    single_hand.draw()
+
     layout.write(
-        "/Users/anders/Documents/Skole/5.host/Lab/FordypningLab/LED_Design.gds"
+        "/Users/anders/Documents/Skole/5.host/Lab/FordypningLab/Single_LED.gds"
+    )
+
+
+def draw_grid_etch_mask(layout, top):
+
+    # top = layout.create_cell("TOP")
+    layer_box = layout.layer(0, 0)
+    layer_txt = layout.layer(1, 0)
+    layer = layout.layer(2, 0)
+
+    scale = 1000
+
+    LED_widht = 1000 * scale  # microns
+    LED_heigth = 1000 * scale  # microns
+
+    LED_spacing_x = 1600 * scale  # microns
+    LED_spacing_y = 1600 * scale  # microns
+
+    finger_widths = np.array([4, 4.5, 5, 5.5]) * scale  # microns
+    finger_pitches = np.array([50, 100, 150, 230]) * scale  # microns
+
+    startpos = Point(0, 0)
+
+    enclosing_box_fraction = 0.3
+
+    for x in range(4):
+        for y in range(4):
+
+            hand_point = startpos.offset(
+                x * (LED_widht + LED_spacing_x), y * (LED_heigth + LED_spacing_y)
+            )
+
+            finger_width = finger_widths[y]
+            finger_pitch = finger_pitches[x]
+
+            single_hand = Hand(
+                top,
+                layer,
+                layer_box,
+                enclosing_box_fraction,
+                hand_point,
+                finger_width,
+                finger_pitch,
+                LED_widht,
+                LED_heigth,
+                scale,
+            )
+
+            single_hand.draw_etch_mask()
+
+
+if __name__ == "__main__":
+
+    layout = pya.Layout()
+    top = layout.create_cell("TOP")
+    draw_grid_etch_mask(layout, top)
+    draw_grid(layout, top)
+    layout.write(
+        "/Users/anders/Documents/Skole/5.host/Lab/FordypningLab/E2_2022_LED_Etch_mask.gds"
     )
